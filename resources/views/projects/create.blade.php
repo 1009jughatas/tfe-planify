082a5fc6cb2cb61dce8d17048226399b650ce5a6
Justin Ghatas <1009jughatas@student.epfc.eu>
2024-09-16 16:09:27 +0200

    modification de création de projet

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,25 +1,25 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create Project') }}
+            {{ __('Créer un projet') }}
         </h2>
     </x-slot>
 
     <div class="container py-12">
         <div class="card">
             <div class="card-header">
-                {{ __('Create New Project') }}
+                {{ __('Créer un nouveau projet') }}
             </div>
             <div class="card-body">
                 <form action="{{ route('projects.store') }}" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label for="name" class="form-label">Project Name</label>
+                        <label for="name" class="form-label">Nom du projet</label>
                         <input type="text" name="name" class="form-control" id="name" value="{{ old('name') }}"
                             required>
                     </div>
                     <div class="mb-3">
-                        <label for="description" class="form-label">Project Description</label>
+                        <label for="description" class="form-label">Description du projet</label>
                         <textarea name="description" class="form-control"
                             id="description">{{ old('description') }}</textarea>
                     </div>
@@ -32,16 +32,16 @@
                         </select>
                     </div>
                     <div class="mb-3">
-                        <label for="start_date" class="form-label">Start Date</label>
+                        <label for="start_date" class="form-label">Date de début</label>
                         <input type="date" name="start_date" class="form-control" id="start_date"
                             value="{{ old('start_date') }}">
                     </div>
                     <div class="mb-3">
-                        <label for="end_date" class="form-label">End Date</label>
+                        <label for="end_date" class="form-label">Date de fin</label>
                         <input type="date" name="end_date" class="form-control" id="end_date"
                             value="{{ old('end_date') }}">
                     </div>
-                    <button type="submit" class="btn btn-primary">Create Project</button>
+                    <button type="submit" class="btn btn-primary">Créer le projet</button>
                 </form>
             </div>
         </div>
